99bc30775fe5949d352e1909506847cc71475d73
Ewcia <ewelinatrzaska@interia.pl>
2020-12-27 15:04:45 +0100

    takeScreenShot method added

--- src/main/java/helpers/CommonUtils.java
+++ src/main/java/helpers/CommonUtils.java
@@ -1,7 +1,16 @@
 package helpers;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import java.io.File;
+import java.io.IOException;
 
 public class CommonUtils {
-   // public static void takeScreanShot(File)
+   public static void takeScreenShot(WebDriver driver)throws IOException{
+       File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+       FileUtils.copyFile(scrFile, new File("screenshit.png"));
+   }
 }
